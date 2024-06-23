3ec8564aa634cbd2d1d5a864b9623406eeba541c
baskivani <burhanzeylan@gmail.com>
2024-06-23 16:51:20 +0300

    Integration youtube with salesforce

--- force-app/main/default/lwc/youtubeLWC/youtubeLWC.js
+++ force-app/main/default/lwc/youtubeLWC/youtubeLWC.js
@@ -1,3 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import getVideos from '@salesforce/apex/YoutubeResult.getVideos';
 
-export default class YoutubeLWC extends LightningElement {}
+export default class YoutubeVideos extends LightningElement {
+    @track videos;
+    @track error;
+
+    @wire(getVideos)
+    wiredVideos({ error, data }) {
+        if (data) {
+            this.videos = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.videos = undefined;
+        }
+    }
+}
